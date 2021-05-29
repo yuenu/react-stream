e13edbeb7b1c70db4c2645363cc0d9855cb8bbc7
yuenu <yuenuni@hotmail.com>
2021-05-29 13:48:32 +0800

    feat: add modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,54 @@
+import Modal from "../Modal";
+import history from "../../history";
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream, deleteStream } from "../../actions";
+import { Link } from 'react-router-dom'
 
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>
+  renderActions() {
+    const { id } = this.props.match.params
+    return (
+      <>
+        <button onClick={() => this.props.deleteStream(id)} className="ui primary button">Delete</button>
+        <Link to="/" className="ui button">Cancel</Link>
+      </>
+    );
+  }
+
+  renderConente() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream";
+    }
+
+    return `Are you sure you want to delete this stream with TITLE:  ${this.props.stream.title}`;
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content={this.renderConente()}
+          action={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
 }
 
-export default StreamDelete
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -18,7 +18,7 @@ const streamReducer = (state = [], action) => {
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload }
     case DELETE_STREAM:
-      return _.omit(state, action.payload.id)
+      return _.omit(state, action.payload)
     default:
       return state;
   }
